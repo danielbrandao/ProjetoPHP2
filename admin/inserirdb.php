a6099c2bb792c41333b254d4574615d928b3e7d1
Daniel Brandao <daniel@saude.pb.gov.br>
2019-01-18 08:40:40 -0300

    alteração inserirdb

--- admin/inserirdb.php
+++ admin/inserirdb.php
@@ -10,7 +10,7 @@ $sql = "INSERT INTO admin VALUES
 $insert = mysqli_query($conexao, $sql);
 
 if(!$insert){
-    echo "Ocorreu um erro ao cadastrar no banco de dados. <a href='?pg=admin/inserir'>Tente Novamente</a>";
+    echo "Ocorreu um erro ao cadastrar no banco de dados. <a href='?pg=inserir'>Tente Novamente</a>";
 }else{
    echo "<h3>Cadastrada com sucesso!</h3>";
 }
